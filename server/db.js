792e3370afa73054d91a3397c181b86ee4043f66
dpk <dpk@gmail.com>
2026-03-17 07:48:47 +0530

    chore: enable mysql keep-alive and connection pooling stability

--- server/db.js
+++ server/db.js
@@ -14,7 +14,11 @@ const pool = mysql.createPool({
   },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000
 });
 
 // Test the connection
